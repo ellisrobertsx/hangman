66e424e4ba327b29000c4375180523fd7aecb2b7
ellisrobertsx <ellisrob1998@hotmail.co.uk>
2024-06-17 16:01:03 +0000

    added 3 lists of potential words that can be used during the game

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+""" List of possible words"""
+comedy_list = ['Step Brothers', 'The Hangover', 'Grown Ups', 'Dodgeball', 'Internship', 'Ted', 'Scary Movie', 'American Pie', 'Due Date', 'The Other Guys']
+action_list = ['James Bond', 'John Wick', 'The Matrix', 'Rambo', 'The Gray Man', 'Deep Water Horizon', 'The Maze Runner', 'Batman', 'Catch me if you can', 'Heat']
+horror_list = ['Saw', 'The Purge', 'Halloween', 'A nightmare on Elm street', 'The Conjuring', 'Psycho', 'Scream', 'Paranormal Activity', 'Alien', 'The Silence of the Lambs']
+
+def select_game_mode():
+    while True:
+        mode = input("Select a game mode: 'comedy', 'action', or 'horror': ").lower()
+        if mode == 
