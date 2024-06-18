15a359a167b5ee54726580efb16fdc0c18020271
ellisrobertsx <ellisrob1998@hotmail.co.uk>
2024-06-18 12:49:02 +0000

    added hangman stages

--- run.py
+++ run.py
@@ -1,10 +1,83 @@
 import random
 
-""" List of possible words"""
+# List of possible words
 comedy_list = ['Step Brothers', 'The Hangover', 'Grown Ups', 'Dodgeball', 'Internship', 'Ted', 'Scary Movie', 'American Pie', 'Due Date', 'The Other Guys']
 action_list = ['James Bond', 'John Wick', 'The Matrix', 'Rambo', 'The Gray Man', 'Deep Water Horizon', 'The Maze Runner', 'Batman', 'Catch me if you can', 'Heat']
 horror_list = ['Saw', 'The Purge', 'Halloween', 'A nightmare on Elm street', 'The Conjuring', 'Psycho', 'Scream', 'Paranormal Activity', 'Alien', 'The Silence of the Lambs']
 
+
+#Max number of guesses allowed 
+max_guesses = 6
+
+# Hangman Stages 
+hangman_stages = [
+    """
+     -----
+     |   |
+         |
+         |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+         |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+     |   |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|   |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+         |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    /    |
+         |
+    =========
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\\  |
+    / \\  |
+         |
+    =========
+    """
+]
+
+
+""" Function allows user to select between 3 different game modes"""
 def select_game_mode():
     while True:
         mode = input("Select a game mode: Press 'A' for action, 'B' for horror, or 'C' for comedy: ").upper()
@@ -15,5 +88,6 @@ def select_game_mode():
         elif mode == 'C':
             return comedy_words
         else:
-            print("Invalid choice. Please select 'comedy', 'action', or 'horror'.")
+            print("Invalid choice. Please press 'A' for action, 'B' for horror, or 'C' for comedy.")
+
 
