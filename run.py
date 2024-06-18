9511c3b6a1c4019a5c880f69547d1987f14638a9
ellisrobertsx <ellisrob1998@hotmail.co.uk>
2024-06-18 12:43:42 +0000

    added select game mode function

--- run.py
+++ run.py
@@ -7,5 +7,13 @@ horror_list = ['Saw', 'The Purge', 'Halloween', 'A nightmare on Elm street', 'Th
 
 def select_game_mode():
     while True:
-        mode = input("Select a game mode: 'comedy', 'action', or 'horror': ").lower()
-        if mode == 
+        mode = input("Select a game mode: Press 'A' for action, 'B' for horror, or 'C' for comedy: ").upper()
+        if mode == 'A':
+            return action_words
+        elif mode == 'B':
+            return horror_words
+        elif mode == 'C':
+            return comedy_words
+        else:
+            print("Invalid choice. Please select 'comedy', 'action', or 'horror'.")
+
